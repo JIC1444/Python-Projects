ed035288aa7e1ecf56ba915e0b28f0d4339564ca
JIC1444 <158279190+JIC1444@users.noreply.github.com>
2024-03-09 14:41:47 +0000

    Update knowledgeGraphNorway.py

--- knowledgeGraphNorway.py
+++ knowledgeGraphNorway.py
@@ -5,50 +5,51 @@ from IPython.display import display
 import networkx as nx
 import plotly.graph_objects as go
 
-#want KG of form: head = bacteria1, relation = frequency , tail = location
+#removing sensistive data names until paper is published.
+#cannot show result of KG until paper published
 
-phylum16S = pd.read_csv("Figures/phylum16S.txt", sep=" ") #importing data grouped by Daphne's fn
-table = display(phylum16S) 
-col_names = list(phylum16S.columns.values) #adds location as a header for the KG
+phylum = pd.read_csv("Figures/XXXXX", sep=" ") 
+table = display(phylum) 
+col_names = list(phylum.columns.values) #adds location as a header for the KG
 
 
-phylum16S_head = []
-phylum16S_relation = np.array([])
-phylum16S_tail = []
-locs = ["ALR","AHR","FAU","GUD","HOG","LAV","OVS","RAM","SKJ","ULV2","VES","VIC"] #VIC only has 7 repeats but rest of them have 8
+phylum_head = []
+phylum_relation = np.array([])
+phylum_tail = []
+locs = [XXXXX] 
 
 #extracting data into suitable format df(3x(95x9)) = df(3x855)
-for u in range(0,94): #appending bacteria phyla for each repeat
+for u in range(0,94): 
   for f in range(0,8):
-    phylum16S_tail.append(locs[f])
-  phylum16S_head = phylum16S_head + col_names
-phylum16S_tail.pop()
+    phylum_tail.append(locs[f])
+  phylum_head = phylum_head + col_names
+phylum_tail.pop()
 p = []
 for n in range(0,94):
   for g in col_names:
-    roe = phylum16S[g][n]
+    roe = phylum[g][n]
     print(roe)
     p.append(roe)
-print(p) #need to run this as sometimes it breaks??
+print(p) 
 
-print("head len",len(phylum16S_head))
+print("head len",len(phylum_head))
 print("p len", len(p))
-print("tail len",len(phylum16S_tail))
+print("tail len",len(phylum_tail))
 
-n = len(phylum16S_head) - len(p)
+n = len(phylum_head) - len(p)
 for i in range(0,n):
-  phylum16S_head.pop()
-N = len(phylum16S_tail) - len(p)
+  phylum_head.pop()
+N = len(phylum_tail) - len(p)
 for l in range(0,N):
-  phylum16S_tail.pop()
+  phylum_tail.pop()
 
-phylum16S_graph = pd.DataFrame({"head":phylum16S_head, "relation":p, "tail":phylum16S_tail})
-phylum16S_graph= phylum16S_graph[phylum16S_graph["relation"] != 0] #kills any rows with 0 as relation
+phylum_graph = pd.DataFrame({"head":phylum16S_head, "relation":p, "tail":phylum16S_tail})
+phylum_graph= phylum_graph[phylum_graph["relation"] != 0] #kills any rows with 0 as relation
 
 
 #could do a graph of a graph? ie to include temp and precip.
 G = nx.Graph() 
-for _, row in phylum16S_graph.iterrows():
+for _, row in phylum_graph.iterrows():
   G.add_edge(row["head"], row["tail"], label=row["relation"], weight = row["relation"])
 
 pos = nx.fruchterman_reingold_layout(G, k=0.5)
@@ -88,7 +89,7 @@ edge_label_trace = go.Scatter(
 )
 
 layout = go.Layout(
-    title="Knowledge Graph of Norway Bacteria Data",
+    title="Knowledge Graph of XXXXX",
     titlefont_size=16,
     title_x=0.5,
     showlegend=False,
